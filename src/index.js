52130b0d9e815c98e1f628c0c6071b8575c2c184
xianshannan <xianshannan@qq.com>
2018-10-18 11:03:19 +0800

    新增createCenter options.shouldRunReducer参数

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import { call, createPut, createGetState } from './util';
-
-export default function createCenter(centers) {
+/**
+ *
+ * @param {async function || ...async function} centers center函数或者center数组函数（async函数），
+ * @param {object} options.shouldRunReducer 默认值是true
+ * action.type匹配到center条件后是否运行reducer，
+ * true时无论是否匹配到center条件都要reducer,
+ * false时只有不匹配到center条件才执行reducer，
+ * 这种情况下center必须返回true（不匹配就返回true）,例如
+ *    function(action){
+ *      switch(action.type){
+ *        case "test":
+ *        const data = await call(fetch,'/api/data');
+ *        await put({ type: 'test',payload: data });
+ *      break;
+ *      default:
+ *       //没匹配到center条件
+ *       //当options.shouldRunReducer为false时，这里应该return true;
+ *       return true;
+ *    }
+ *  }
+ *  shouldRunReducer=false这种模式比较容易因为没返回true，就会导致没执行到reducer，出现bug。
+ *  而且当cener条件匹配到，然后没返回true的话，redux-logger这些日志middleware是无法监控到center的日志的。
+ *  看情况场景使用。
+ */
+export default function createCenter(centers, options = {}) {
   if (centers === undefined) {
     //默认值
     centers = async function() {
@@ -11,6 +34,8 @@ export default function createCenter(centers) {
     //先统一处理成数组
     centers = [centers];
   }
+  //shouldRunReducer是否默认运行reducer，默认为true
+  const { shouldRunReducer = true } = options;
   /**
    * redux-center是redux异步操作中间件，
    * redux-center可以简单理解为redux-thunk的升级版，进行了用法规范，
@@ -28,6 +53,7 @@ export default function createCenter(centers) {
    *       await put({ type: 'test',payload: data });
    *     break;
    *     default:
+   *        //当options.shouldRunReducer为false时，这里应该return true;
    *   }
    * }
    * 如果返回true，就会正常运行原来的dispatch
@@ -42,7 +68,10 @@ export default function createCenter(centers) {
      */
     createCenterMiddleware() {
       return ({ dispatch, getState }) => next => async action => {
-        const result = next(action);
+        let result;
+        if (shouldRunReducer) {
+          result = next(action);
+        }
         const promises = centers.map(function(center) {
           //center的元素必须是函数。
           if (typeof center !== 'function') {
@@ -57,7 +86,17 @@ export default function createCenter(centers) {
             });
           }
         });
-        await Promise.all(promises);
+        let shouldRunRunReducerAnterCenters = await Promise.all(promises).then(
+          shouldRunNexts => {
+            return shouldRunNexts.every(shouldRunNext => {
+              return shouldRunNext === true;
+            });
+          }
+        );
+        if (shouldRunRunReducerAnterCenters && !shouldRunReducer) {
+          //根据所有center的返回值决定是否运行reducer，使用者自己决定。
+          result = next(action);
+        }
         return result;
       };
     }
